a0020bc15c8e6469df139edde52f46232f697c4f
Frank Alimimian <alimimianfrank@gmail.com>
2023-04-03 10:29:22 +0100

    Edit week_3/oop.py

--- week_3/oop.py
+++ week_3/oop.py
@@ -161,70 +161,122 @@
 # print(jerry.leave_request(10))
 # #output: Jerry .K Requesting for 10days of leave
 
-#-----------------------INHERITANCE AND MULTIPLE INHERITANCE
+# #-----------------------INHERITANCE AND MULTIPLE INHERITANCE
 
-class A: #class A is the parent class
-    pass
-class B(A):#class B is the child class and inherits the attributes and behaviors of class A
-    pass
+# class A: #class A is the parent class
+#     pass
+# class B(A):#class B is the child class and inherits the attributes and behaviors of class A
+#     pass
 
-#----------------MULTIPLE INHERITANCE
-class A:#first this super class
-    a = 1
-class B:#A super class
-    b = 2
-class C(A,B):#class c inherits from both class A,  and class B
-    pass
-c = C()#instantiate an object c that inherits from the class C, which intone inherited it's properties from class A and B
-print(c.a)
-#output: 1
-print(c.b)
-#output: 2
+# #----------------MULTIPLE INHERITANCE
+# class A:#first this super class
+#     a = 1
+# class B:#A super class
+#     b = 2
+# class C(A,B):#class c inherits from both class A,  and class B
+#     pass
+# c = C()#instantiate an object c that inherits from the class C, which intone inherited it's properties from class A and B
+# print(c.a)
+# #output: 1
+# print(c.b)
+# #output: 2
 
-#-------------MULTI-LEVEL INHERITANCE
-class A:    
-    a = 1    
-class B(A):
-    a = 2   
-class C(B):
-    pass
-c = C()
-print(c.a)  #the out put will be 2 because c inheritance is directly from Class C, which inherited from class B
+# #-------------MULTI-LEVEL INHERITANCE
+# class A:    
+#     a = 1    
+# class B(A):
+#     a = 2   
+# class C(B):
+#     pass
+# c = C()
+# print(c.a)  #the out put will be 2 because c inheritance is directly from Class C, which inherited from class B
 
 
-#-------------------BUILT-IN FUNCTIONS
-'''
-There are two useful function use to check the inheritance in python. The issubclass()  and isinstance()function
-issubclass(): it takes two classes as arguments. it checks if the first class is a child of the second class. 
-              Then return true or false based on the result.
+# #-------------------BUILT-IN FUNCTIONS
+# '''
+# There are two useful function use to check the inheritance in python. The issubclass()  and isinstance()function
+# issubclass(): it takes two classes as arguments. it checks if the first class is a child of the second class. 
+#               Then return true or false based on the result.
 
-'''
-print(issubclass( A,B))#This will return False because A is not a subclass of B
-#output: False
-print(issubclass(B,A))#this will return True because B is a subclass of A
+# '''
+# print(issubclass( A,B))#This will return False because A is not a subclass of B
+# #output: False
+# print(issubclass(B,A))#this will return True because B is a subclass of A
 
-'''
-isinstance(): it works the same way as issubclass(),but determines if an object is an instance of a class.
+# '''
+# isinstance(): it works the same way as issubclass(),but determines if an object is an instance of a class.
 
-'''
-print(isinstance(c, C))#this this returns True, because c is an instance of the class C.
+# '''
+# print(isinstance(c, C))#this this returns True, because c is an instance of the class C.
 
 #-------Super():
-'''
-Super() function is a built-in function that can be called inside the derived class and give access to the methods
-and variables of the parent classes or siblings. When you call the super() function, you get an object that 
-represent the parent class in return.
-'''
-class Fruit():
-    def __init__(self, fruit) -> None:
-        print('Fruit type: ' ,fruit)
+# '''
+# Super() function is a built-in function that can be called inside the derived class and give access to the methods
+# and variables of the parent classes or siblings. When you call the super() function, you get an object that 
+# represent the parent class in return.
+# '''
+# class Fruit():
+#     def __init__(self, fruit) -> None:
+#         print('Fruit type: ' ,fruit)
 
-class FruitFlavour(Fruit):
-    def __init__(self) -> None:
-        super().__init__("Apple")#this is use to initialize the base class in side the child class so it get access to
-                                #the method.
-        print("Apple is sweet")
+# class FruitFlavour(Fruit):
+#     def __init__(self) -> None:
+#         super().__init__("Apple")#this is use to initialize the base class in side the child class so it get access to
+#                                 #the method.
+#         print("Apple is sweet")
 
-apple = FruitFlavour()
-#output:Fruit type:  Apple
-#output: Apple is sweet
+# apple = FruitFlavour()
+# #output:Fruit type:  Apple
+# #output: Apple is sweet
+
+#--------------------METHOD RESOLUTION ORDER---------------------------------------------
+'''
+In the case where there is multiple inheritance. MRO helps to resolve the issues of inheritance conflicts.
+there are four major types of inheritance:
+i.simple inheritance: Just a parent -> child inheritance.
+ii.multiple inheritance: this is when a child class inherit from different parents.
+iii.multi-level inheritance: this is when inheritance happens in multiple levels. like a child inheriting from a subclass
+that subclass inheriting from other parents. The chain can go on and on
+iv.Hierarchical inheritance: this handles how several subclass inherit from one and other.
+There is a fifth type called HYBRID-INHERITANCE which combines the characters from the other types of inheritance.
+MRO: It determines the order a given method or attribute is passed through in a search ot the hierarchy of classes for  it's resolution. The order of resolution is 
+called LINEARIZATION OF A CLASS. it follows a simple logic bottom to top, left to right. The older version of python uses a 
+DEPTH-FIRST SEARCH ALGORITHM(DFS) while recent version use C3 LINEARIZATION ALGORITHM.
+MRO() FUNTIONS:There are two functions to check the inheritance of class :
+mro(): shows the inheritance graph of the classes
+help(): Provides more details with the mro() information at the top
+''' 
+#Example    
+class A:
+    def d(self):
+        return "Function inside A"
+
+class B:
+    def d(self):
+        return "Function inside B"
+
+
+class C:
+    def d(self):
+        return "Function inside C"
+
+
+class D(A, B):
+    def d(self):
+        return "Function inside D"
+
+
+class E(B, C):
+    def d(self):
+        return "Function inside E"
+
+
+class F(E,D,C):
+    pass
+
+f = F()
+print(f.d())
+#output: Function inside E
+print(F.mro())
+#output:[<class '__main__.F'>, <class '__main__.E'>, <class '__main__.D'>, <class '__main__.A'>, <class '__main__.B'>, 
+# <class '__main__.C'>, <class 'object'>]
